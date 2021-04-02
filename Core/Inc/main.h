7115cfc157e5da5782a8cb4fd7dbe5f290dc6d6d
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2021-04-02 21:37:23 +0200

    Init
    # Liste des mots clés de titre de commit (version courte -> résultat long)
    # r <num> -> Récupération du Redmine (Redmine #<num> : <titre_du_redmine>)
    # t <titre> -> Technique : <titre>
    # f <titre> -> Fix : <titre>
    # ty <titre> -> Typo : <titre>
    # d <titre> -> Doc : <titre>
    # s <titre> -> Sécurité : <titre>
    # u <titre> -> UI : <titre>
    # c <titre> -> CI : <titre>
    # WIP <desc>
    # [skip ci] en début de titre de commit permet de ne pas déclencher de build
    #
    # Chaque ligne de description ajouté sera préfixé automatiquement d'une puce.
    #
    # Pour la saisie de temps automatique lors de l'écriture d'un commit le format est le suivant :
    # /spend <options>
    #
    # NB : Plusieurs /spend peuvent être défini sur un même commit
    #
    # Les options sont :
    # - Temps en heure (ie 0.25). Obligatoire
    # - Numéro de redmine. Facultatif si le commit porte sur un redmine avec la syntaxe courte ou longue.
    # - Catégorie de l'imputation. Facultatif. Récupération par les premières lettres en format court, si non défini Développement sera utilisé
    # - Date de l'imputation au format ISO8601 (ie 2021-02-23)
    #
    # NB : L'imputation n'est pas bloquante pour le message de commit si une erreur survient.

---
+++ Core/Inc/main.h
@@ -0,0 +1,119 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define VAC_1_Pin GPIO_PIN_0
+#define VAC_1_GPIO_Port GPIOA
+#define VAC_2_Pin GPIO_PIN_1
+#define VAC_2_GPIO_Port GPIOA
+#define VAC_3_Pin GPIO_PIN_2
+#define VAC_3_GPIO_Port GPIOA
+#define VAC_4_Pin GPIO_PIN_3
+#define VAC_4_GPIO_Port GPIOA
+#define STBY_1_Pin GPIO_PIN_4
+#define STBY_1_GPIO_Port GPIOA
+#define PUMP_1_Pin GPIO_PIN_5
+#define PUMP_1_GPIO_Port GPIOA
+#define EV_1_Pin GPIO_PIN_6
+#define EV_1_GPIO_Port GPIOA
+#define STBY_2_Pin GPIO_PIN_7
+#define STBY_2_GPIO_Port GPIOA
+#define PUMP_2_Pin GPIO_PIN_0
+#define PUMP_2_GPIO_Port GPIOB
+#define EV_2_Pin GPIO_PIN_1
+#define EV_2_GPIO_Port GPIOB
+#define BOOT_1_Pin GPIO_PIN_2
+#define BOOT_1_GPIO_Port GPIOB
+#define STBY_3_Pin GPIO_PIN_10
+#define STBY_3_GPIO_Port GPIOB
+#define PUMP_3_Pin GPIO_PIN_11
+#define PUMP_3_GPIO_Port GPIOB
+#define EV_3_Pin GPIO_PIN_12
+#define EV_3_GPIO_Port GPIOB
+#define STBY_4_Pin GPIO_PIN_13
+#define STBY_4_GPIO_Port GPIOB
+#define PUMP_4_Pin GPIO_PIN_14
+#define PUMP_4_GPIO_Port GPIOB
+#define EV_4_Pin GPIO_PIN_15
+#define EV_4_GPIO_Port GPIOB
+#define PRES_4_Pin GPIO_PIN_8
+#define PRES_4_GPIO_Port GPIOA
+#define PRES_3_Pin GPIO_PIN_9
+#define PRES_3_GPIO_Port GPIOA
+#define PRES_2_Pin GPIO_PIN_10
+#define PRES_2_GPIO_Port GPIOA
+#define PRES_1_Pin GPIO_PIN_11
+#define PRES_1_GPIO_Port GPIOA
+#define HEART_BEAT_Pin GPIO_PIN_12
+#define HEART_BEAT_GPIO_Port GPIOA
+#define ADD_2_Pin GPIO_PIN_4
+#define ADD_2_GPIO_Port GPIOB
+#define ADD_1_Pin GPIO_PIN_5
+#define ADD_1_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ Core/Src/gpio.c
@@ -0,0 +1,110 @@
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+        * Free pins are configured automatically as Analog (this feature is enabled through
+        * the Code Generation settings)
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, STBY_1_Pin|PUMP_1_Pin|EV_1_Pin|STBY_2_Pin
+                          |PRES_4_Pin|PRES_3_Pin|PRES_2_Pin|PRES_1_Pin
+                          |HEART_BEAT_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, PUMP_2_Pin|EV_2_Pin|STBY_3_Pin|PUMP_3_Pin
+                          |EV_3_Pin|STBY_4_Pin|PUMP_4_Pin|EV_4_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PC13 PC14 PC15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PAPin PAPin PAPin PAPin
+                           PAPin PAPin PAPin PAPin
+                           PAPin */
+  GPIO_InitStruct.Pin = STBY_1_Pin|PUMP_1_Pin|EV_1_Pin|STBY_2_Pin
+                          |PRES_4_Pin|PRES_3_Pin|PRES_2_Pin|PRES_1_Pin
+                          |HEART_BEAT_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PBPin PBPin PBPin PBPin
+                           PBPin PBPin PBPin PBPin */
+  GPIO_InitStruct.Pin = PUMP_2_Pin|EV_2_Pin|STBY_3_Pin|PUMP_3_Pin
+                          |EV_3_Pin|STBY_4_Pin|PUMP_4_Pin|EV_4_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PBPin PBPin PBPin */
+  GPIO_InitStruct.Pin = BOOT_1_Pin|ADD_2_Pin|ADD_1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB8 PB9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 2 */
+
+/* USER CODE END 2 */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
